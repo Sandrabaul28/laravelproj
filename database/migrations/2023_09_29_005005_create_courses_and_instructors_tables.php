0a54bf22139768376e22dc479071d08e9d9ea71a
Sandrabaul28 <wabinasandra@gmail.com>
2023-09-29 02:51:37 +0100

    added activity # 4

---
+++ database/migrations/2023_09_29_005005_create_courses_and_instructors_tables.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    //Schema for the "courses" table
+    public function up(): void
+    {
+        Schema::create('courses', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->timestamps();
+        });
+        
+        Schema::create('instructors', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->timestamps(); 
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('courses_and_instructors_tables');
+    }
+};
